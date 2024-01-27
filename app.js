1fecf6b5f226b6ff6d566a3456e9ea44ba71ae70
baicorp <bagusatoki@gmail.com>
2024-01-27 13:46:19 +0700

    .

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+
 const app = express();
-const port = 3000;
+const port = 5000;
 app.use(cors());
 
 // main entry
