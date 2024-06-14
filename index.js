1a8867106ee63aa6073932f8743d1f8ec80c5fc9
baicorp <bagusatoki@gmail.com>
2024-06-15 00:31:51 +0700

    add watch endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import fetch from "node-fetch";
 import { Innertube } from "youtubei.js";
 
 const app = express();
@@ -45,6 +46,64 @@ app.post("/stream", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "internal setver error" });
   }
+});
+
+app.post("/watch", async (req, res) => {
+  const videoId = req.query.videoId;
+
+  if (!videoId) {
+    return res
+      .status(400)
+      .json({ error: "videoId query parameter is required" });
+  }
+  const response = await fetch(
+    "https://music.youtube.com/youtubei/v1/player?key=AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent":
+          "com.google.android.youtube/17.36.4 (Linux; U; Android 12; GB) gzip",
+        Cookie:
+          "CONSENT=YES+; SOCS=CAISNQgDEitib3FfaWRlbnRpdHlmcm9udGVuZHVpc2VydmVyXzIwMjMwODI5LjA3X3AxGgJlbiACGgYIgLC_pwY",
+      },
+      body: JSON.stringify({
+        videoId,
+        context: {
+          client: {
+            clientName: "ANDROID_TESTSUITE",
+            clientVersion: "1.9",
+            androidSdkVersion: 30,
+            hl: "en",
+            gl: "US",
+            utcOffsetMinutes: 0,
+          },
+        },
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(`YouTube API Error: ${errorData.error.message}`);
+  }
+
+  const data = await response.json();
+
+  const thumbnails = data?.videoDetails?.thumbnail?.thumbnails[0]?.url;
+  const result = {
+    videoId: data?.videoDetails?.videoId,
+    title: data?.videoDetails?.title,
+    thumbnailUrl: thumbnails,
+    channelId: data?.videoDetails?.channelId,
+    uploader: data?.videoDetails?.author,
+    videoDetails: { ...data?.videoDetails, thumbnail: thumbnails },
+    url: data.streamingData?.adaptiveFormats[
+      data.streamingData?.adaptiveFormats?.length - 1
+    ]?.url,
+  };
+
+  res.send(result?.url);
 });
 
 app.listen(port, () => {
