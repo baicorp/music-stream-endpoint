f4bf4b7c360f2aa2698db90e74cb32d6f2fcbba9
baicorp <bagusatoki@gmail.com>
2024-06-16 01:52:25 +0700

    proxy

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
+import axios from "axios";
+import bodyParser from "body-parser";
 import { Innertube } from "youtubei.js";
 
 const app = express();
 const port = 5000;
 
+app.use(bodyParser.json());
 app.use(express.json());
 
 app.use(cors());
@@ -105,6 +108,98 @@ app.post("/watch", async (req, res) => {
   };
 
   res.send(result?.url);
+});
+
+const baseURL = "https://music.youtube.com";
+const apiKey = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8";
+
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    "X-Goog-Api-Key": apiKey,
+  },
+  params: {
+    prettyPrint: "false",
+  },
+});
+
+const endpoints = {
+  player: "/youtubei/v1/player",
+};
+
+async function fetchPlayer(body) {
+  try {
+    const response = await axiosInstance.post(endpoints.player, body, {
+      headers: {
+        "X-Goog-Api-Key": apiKey,
+      },
+    });
+
+    if (response.data.playabilityStatus.status === "OK") {
+      return response.data;
+    } else {
+      const safeBody = {
+        ...body,
+        context: {
+          ...body.context,
+          thirdParty: {
+            embedUrl: `https://www.youtube.com/watch?v=${body.videoId}`,
+          },
+        },
+      };
+
+      const safeResponse = await axiosInstance.post(endpoints.player, safeBody);
+
+      if (safeResponse.data.playabilityStatus.status !== "OK") {
+        return response.data;
+      }
+
+      const audioStreamsResponse = await axios.get(
+        `https://watchapi.whatever.social/streams/${body.videoId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const audioStreams = audioStreamsResponse.data.audioStreams;
+
+      const updatedFormats =
+        safeResponse.data.streamingData.adaptiveFormats.map((format) => {
+          const matchedStream = audioStreams.find(
+            (stream) => stream.bitrate === format.bitrate
+          );
+          return {
+            ...format,
+            url: matchedStream ? matchedStream.url : format.url,
+          };
+        });
+
+      return {
+        ...safeResponse.data,
+        streamingData: {
+          ...safeResponse.data.streamingData,
+          adaptiveFormats: updatedFormats,
+        },
+      };
+    }
+  } catch (error) {
+    console.error("Error fetching player data:", error);
+    throw error;
+  }
+}
+
+app.post("/proxy/player", async (req, res) => {
+  try {
+    const playerData = await fetchPlayer(req.body);
+    res.json(playerData);
+  } catch (error) {
+    res
+      .status(error.response ? error.response.status : 500)
+      .send(error.message);
+  }
 });
 
 app.listen(port, () => {
